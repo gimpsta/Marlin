fe9e34ca57a97dbf3994f623321503525845ea6f
gIMpSTa <gimpsta@gmail.com>
2023-12-01 11:03:52 -0500

    fix x probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1574,7 +1574,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -8, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -8, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
