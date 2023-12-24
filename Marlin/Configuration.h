6b4fbc6200bfab2ddafc0729d87fea9b570fc47a
gIMpSTa <gimpsta@gmail.com>
2023-12-23 22:43:09 -0500

    update bed and nozzle pids

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -693,9 +693,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp  13.88
+    #define DEFAULT_Ki   0.69
+    #define DEFAULT_Kd  70.21
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
@@ -782,9 +782,9 @@
   //#define PID_BED_DEBUG // Print Bed PID debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
+  #define DEFAULT_bedKp  98.17
+  #define DEFAULT_bedKi  17.28
+  #define DEFAULT_bedKd 371.73
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
