778160993d9491cfa5f2466b524cc56ce9da2aca
gIMpSTa <gimpsta@gmail.com>
2023-12-25 23:17:22 -0500

    tune thermal runaway protection

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -307,7 +307,7 @@
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
   #define THERMAL_PROTECTION_PERIOD 40        // (seconds)
-  #define THERMAL_PROTECTION_HYSTERESIS 5     // (°C)
+  #define THERMAL_PROTECTION_HYSTERESIS 15     // (°C)
 
   #define ADAPTIVE_FAN_SLOWING              // Slow down the part-cooling fan if the temperature drops
   #if ENABLED(ADAPTIVE_FAN_SLOWING)
